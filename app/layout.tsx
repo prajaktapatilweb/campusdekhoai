b6e32a44cd583af1cbdecc0786898d2cb9c4c79f
Prajakta_Laptop <prajaktaweb@gmail.com>
2026-06-02 10:46:38 +0530

    Analytics code updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,54 @@ export default function RootLayout({
         <link rel="icon" href="/images/pudharilogo.png" />
       </Head> */}
       <body className="font-sans antialiased">
-        <script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4B48SS8PS5"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-4B48SS8PS5');
+    `}
+        </Script>
+
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;
+      n.push=n;
+      n.loaded=!0;
+      n.version='2.0';
+      n.queue=[];
+      t=b.createElement(e);
+      t.async=!0;
+      t.src=v;
+      s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s);
+      }(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '1757594748940042');
+      fbq('track', 'PageView');
+    `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1757594748940042&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* <script>
           {`!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -92,6 +139,19 @@ fbq('track', 'PageView');`}
             src="https://www.facebook.com/tr?id=1757594748940042&ev=PageView&noscript=1"
           />
         </noscript>
+         */}
+        {/* <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4B48SS8PS5"
+          strategy="afterInteractive"
+        />
+        <script>
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4B48SS8PS5');`}
+        </script> */}
 
         <LanguageProvider>
           <AuthProvider>
